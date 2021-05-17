ac002b101952a5493d9761cc42ef2bfed670ef41
LorisDonovan <a7shky@gmail.com>
2021-05-17 08:46:48 +0545

    update

--- utils.py
+++ utils.py
@@ -14,12 +14,14 @@ def extractTensors(experiences):
 	t4 = torch.cat(batch.nextState)
 	return (t1, t2, t3, t4)
 
+
 def preprocessing(img):
 	img = cv2.cvtColor(cv2.resize(img, (84, 110)), cv2.COLOR_BGR2GRAY)
 	img = img[18:102, ] # crop
 	# _, img = cv2.threshold(img, 10, 255, cv2.THRESH_BINARY)
 	# return np.reshape(img, (1, 1, 84, 84))
 	return torch.tensor(img/255).reshape(1, 1, 84, 84).to(torch.float32)
+
 
 def plotSubplot(frames = None, num = 4, nrow = 1, ncol = 4, titles = None):
 	for i in range(num):
@@ -28,6 +30,7 @@ def plotSubplot(frames = None, num = 4, nrow = 1, ncol = 4, titles = None):
 			plt.title(titles[i])
 		plt.imshow(frames[i].reshape(84, 84), 'gray')
 	plt.show()
+
 
 # # maybe i wont use this function
 # def frameStacking(observation, lastAction, env, numFrames=4):
